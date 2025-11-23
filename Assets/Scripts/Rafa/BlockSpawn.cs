2cebb1a7dc93755edffa4b2c0c4124ce8dfe07f2
CrimsonsTempest <rafaeldevenang@gmail.com>
2025-11-23 11:01:39 +0700

    prefab fix spawnpoint and rotation

--- Assets/Scripts/Rafa/BlockSpawn.cs
+++ Assets/Scripts/Rafa/BlockSpawn.cs
@@ -8,6 +8,9 @@ public class BlockSpawn : MonoBehaviour
     [SerializeField] ListofBlocks normal;
 
     public static BlockSpawn instance;
+    public float spawnInterval = 1.5f;
+
+     private
     void Awake()
     {
         instance = this;
@@ -18,7 +21,7 @@ float curtime=0;
     void Update()
     {
         curtime+=Time.deltaTime;
-        if (curtime >= 5)
+        if (curtime >= spawnInterval)
         {
             SpawnBlock();
             curtime=0;
@@ -30,8 +33,21 @@ float curtime=0;
         GameObject blockToSpawn = normal.GetRandomBlock();
         if (blockToSpawn != null)
         {
-            int random = Random.Range(0, 13);
+            int random = Random.Range(0, 12);
             Vector2 position = new Vector2(random, transform.position.y);
+            Instantiate(blockToSpawn,position, blockToSpawn.transform.rotation);
+        }
+    }
+
+    void SpawnBlockFol()
+    {
+        GameObject Player = GameObject.FindGameObjectWithTag("Player");
+        Vector2 playerPos = Player.transform.position;
+        GameObject blockToSpawn = normal.GetRandomBlock();
+        if (blockToSpawn != null)
+        {
+            int x = (int)playerPos.x;
+            Vector2 position = new Vector2(x, transform.position.y);
             Instantiate(blockToSpawn,position, Quaternion.identity);
         }
     }
