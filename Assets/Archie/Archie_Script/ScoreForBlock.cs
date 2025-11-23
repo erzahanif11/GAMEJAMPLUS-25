81ca907648b66e5c4aa681de9f799f846f0b6701
Archie <57532087+Rockblank@users.noreply.github.com>
2025-11-23 20:55:36 +0700

    Score System per Stacked

--- Assets/Archie/Archie_Script/ScoreForBlock.cs
+++ Assets/Archie/Archie_Script/ScoreForBlock.cs
@@ -2,15 +2,18 @@ using UnityEngine;
 
 public class ScoreForBlock : MonoBehaviour
 {
-    public int ScorePerStack = 5;
+    public int scorePerStack = 5;
     public bool hasStacked = false;
 
     void OnTriggerEnter2D(Collider2D collision)
     {
-        if(collision.gameObject.CompareTag("Block") && !hasStacked)
+        if(collision.gameObject.CompareTag("Block") && !hasStacked && ScoreManager.Instance != null)
         {
             hasStacked = true;
 
+            ScoreManager.Instance.AddScore(scorePerStack);
+
+            Debug.Log("Tumpukan Blok Terdeteksi! Score +5.");
         }
     }
 }
