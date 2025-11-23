946c039e8a1c7c3c373b294b68af6bd70d2dbf76
CrimsonsTempest <rafaeldevenang@gmail.com>
2025-11-23 23:38:42 +0700

    r453t5y

--- Assets/Scripts/GS/Player Movement.cs
+++ Assets/Scripts/GS/Player Movement.cs
@@ -41,7 +41,7 @@ public class PlayerMovement : MonoBehaviour
     float originalSpeed;
     float originalJump;
     Coroutine speedEffectCoroutine;
-  //  Animator animator;
+   Animator animator;
 
     public float footdistanceonground=0.5f;
     public float footdistancemidair=0.5f;
@@ -131,15 +131,17 @@ public class PlayerMovement : MonoBehaviour
         {
             WallJump();
         }
+
+            animator.SetFloat("VelocityX", Mathf.Abs(rb.linearVelocity.x));
+        animator.SetFloat("VelocityY", rb.linearVelocity.y);
+        animator.SetBool("isGrounded", isGrounded);
     }
 
     void FixedUpdate()
     {
         
 
-        // animator.SetFloat("Velocity", Mathf.Abs(rb.linearVelocity.x));
-        // animator.SetFloat("VelocityY", rb.linearVelocity.y);
-        // animator.SetBool("isGrounded", isGrounded);
+    
         // animator.SetBool("isSliding", (isSliding&&!isGrounded));
         // animator.SetBool("hug wall", groundedLeft||groundedRight);
 
@@ -203,11 +205,11 @@ public class PlayerMovement : MonoBehaviour
     void Flip()
     {
         float scaleX = Mathf.Abs(transform.localScale.x);
-        if (moveInput > 0)
+        if (moveInput < 0)
         {
             transform.localScale = new Vector3(scaleX, transform.localScale.y, transform.localScale.z);
         }
-        else if (moveInput < 0)
+        else if (moveInput > 0)
         {
             transform.localScale = new Vector3(-scaleX, transform.localScale.y, transform.localScale.z);
         }
