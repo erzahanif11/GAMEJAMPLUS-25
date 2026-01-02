db848e54330f80e4dfd2c4586d03ba75035f303f
Archie <57532087+Rockblank@users.noreply.github.com>
2026-01-02 20:38:13 +0700

    Weighted Random
    adding weighted random to spawn system

--- Assets/Scenes/Archie/Archie's Script/Spawner.cs
+++ Assets/Scenes/Archie/Archie's Script/Spawner.cs
@@ -2,8 +2,17 @@ using UnityEngine;
 
 public class Spawner : MonoBehaviour
 {
+    [Header("General Settings")]
     [SerializeField] GameObject enemyPrefab;
     [SerializeField] float spawnCD;
+
+    [Space]
+
+    [Header("Attack Type Settings")]
+    [SerializeField] int normalRate;
+    [Tooltip("Please Note! Divide wisely with the locking rate to keep total of em 100")]
+    [SerializeField] int lockingRate;
+    [Tooltip("Please Note! Divide wisely with the locking rate to keep total of em 100")]
     [SerializeField] SpawnDir direction;
 
     GameObject playerObj;
@@ -33,7 +42,7 @@ public class Spawner : MonoBehaviour
 
     void spawn()
     {
-        int typeAtk = Random.Range(0,2);
+        int typeAtk = Random.Range(0,101);
         Debug.Log("Type atk is: " + typeAtk);
         Vector2 spawnPos = Vector2.zero;
         Bounds cBound = bCollider.bounds;
@@ -41,33 +50,33 @@ public class Spawner : MonoBehaviour
         switch(direction)
         {
             case SpawnDir.Down:
-                if(typeAtk == 0)
+                if(typeAtk >= 0 && typeAtk <= normalRate)
                 {
                     spawnPos.x = Random.Range(cBound.min.x, cBound.max.x);
                     spawnPos.y = cBound.min.y;
-                } else if(typeAtk == 1)
+                } else if(typeAtk >= (normalRate + 1) && typeAtk <= 100) 
                 {
                     spawnPos.x = playerObj.transform.position.x;
                     spawnPos.y = cBound.min.y;
                 }
                 break;
             case SpawnDir.Left:
-                if(typeAtk == 0)
+                if(typeAtk >= 0 && typeAtk <= normalRate)
                 {
                     spawnPos.x = cBound.min.x;
                     spawnPos.y = Random.Range(cBound.min.y, cBound.max.y);
-                } else if(typeAtk == 1)
+                } else if(typeAtk >= (normalRate + 1) && typeAtk <= 100) 
                 {
                     spawnPos.x = cBound.min.x;
                     spawnPos.y = playerObj.transform.position.y;
                 }
                 break;
             case SpawnDir.Right:
-                if(typeAtk == 0)
+                if(typeAtk >= 0 && typeAtk <= normalRate)
                 {
                     spawnPos.x = cBound.max.x;
                     spawnPos.y = Random.Range(cBound.min.y, cBound.max.y);
-                } else if(typeAtk == 1)
+                } else if(typeAtk >= (normalRate + 1) && typeAtk <= 100) 
                 {
                     spawnPos.x = cBound.max.x;
                     spawnPos.y = playerObj.transform.position.y;
