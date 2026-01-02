cbe7d191020bee55b5a91b30517dc6f5d68ca990
Archie <57532087+Rockblank@users.noreply.github.com>
2026-01-02 13:10:06 +0700

    Test Environment
    Add: Train folder, Dev Folder, Feature folder, test scene, test script

---
+++ Assets/Scenes/Archie/Archie's Script/MovementTest.cs
@@ -0,0 +1,20 @@
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class MovementTest : MonoBehaviour
+{
+    [SerializeField] float speed;
+    Rigidbody2D rb;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rb.linearVelocity = new Vector2(Input.GetAxis("Horizontal")*speed, 0f);
+    }
+}
