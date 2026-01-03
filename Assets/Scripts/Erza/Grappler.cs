5748d67fb547ece21a09422b0712206f7ad1be6e
erzahanif11 <erzahanif11@gmail.com>
2026-01-03 10:01:41 +0700

    Pasang animation ke main scene

--- Assets/Scripts/Erza/Grappler.cs
+++ Assets/Scripts/Erza/Grappler.cs
@@ -16,10 +16,10 @@ public class Grappler : MonoBehaviour
     }
 
     void Update(){
-        if (Input.GetMouseButtonDown(0)){
+        if (Input.GetMouseButtonDown(1)){
             StartGrapple();
         }
-        else if (Input.GetMouseButtonUp(0)){
+        else if (Input.GetMouseButtonUp(1)){
             StopGrapple();
         }
         if (isGrappling){
